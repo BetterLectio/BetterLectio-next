87f5d9d8c6f230a61664177abc2cd78ba8077fcc
Johan Ohly <johanohly@gmail.com>
2024-06-21 01:11:20 +0200

    fix: proper mobile drawer trigger button width

--- src/lib/components/SiteHeader.svelte
+++ src/lib/components/SiteHeader.svelte
@@ -43,7 +43,7 @@
 		? 'border-background'
 		: 'border-b'} border-border bg-background shadow-sm flex"
 >
-	<div class="w-[72px] flex items-center justify-center shrink-0" data-tauri-drag-region>
+	<div class="max-sm:pl-3 max-sm:pr-1 sm:w-[72px] flex items-center justify-center shrink-0" data-tauri-drag-region>
 		<Button on:click={toggleSidebar} class="size-8" variant="ghost" size="icon">
 			<AlignJustify class="size-5" />
 			<span class="sr-only">Skjul/vis sidepanel</span>
