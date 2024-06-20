ba7b8e367f182e68163a608116ee4c9e1fa34c21
Johan Ohly <johanohly@gmail.com>
2024-06-21 01:00:53 +0200

    chore: use tailwind breakpoints instead of viewport width in assignments page

--- src/routes/opgaver/+page.svelte
+++ src/routes/opgaver/+page.svelte
@@ -1,3 +1,4 @@
+
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { Spinner } from '$lib/components';
@@ -42,11 +43,6 @@
 		search();
 	}
 
-	$: viewportWidth = window.innerWidth;
-	addEventListener('resize', () => {
-		viewportWidth = window.innerWidth;
-	});
-
 	function search() {
 		const searchResults: RawSimpleAssignment[] = [];
 		opgaver?.forEach((opgave) => {
@@ -88,186 +84,191 @@
 		</div>
 	</div>
 
-	{#if viewportWidth > 768}
-		<Table.Root class="overflow-x-auto" containerClass="!mt-4">
-			<Table.Header>
-				<Table.Row>
-					<Table.Head></Table.Head>
-					<Table.Head class="sm:table-cell">Elev tid</Table.Head>
-					<Table.Head></Table.Head>
-					<Table.Head class="sm:table-cell">Hold</Table.Head>
-					<Table.Head class="sm:table-cell">Frist</Table.Head>
-					<Table.Head class="sm:table-cell">Opgavetitel</Table.Head>
-				</Table.Row>
-			</Table.Header>
-			<Table.Body>
-				{#if opgaver}
-					{#each opgaver as opgave}
-						<Table.Row
-							on:click={async () => await goto(`/opgave?id=${opgave.exerciseid}`)}
-							class="cursor-pointer select-none"
-						>
-							<!-- handed in -->
-							<div class="flex items-center ml-2 h-9">
-								<Tooltip.Root>
-									{#if opgave.status === 'Afleveret'}
-										<Tooltip.Trigger
-											><Rocket
-												class="w-4 h-4 text-green-800 dark:text-green-400"
-											/></Tooltip.Trigger
-										>
-										<Tooltip.Content>
-											<p>Afleveret</p>
-										</Tooltip.Content>
-									{:else if opgave.status === 'Afsluttet'}
-										<Tooltip.Trigger
-											><Archive class="w-4 h-4 text-blue-800 dark:text-blue-400" /></Tooltip.Trigger
-										>
-										<Tooltip.Content>
-											<p>Afsluttet</p>
-										</Tooltip.Content>
-									{:else if opgave.status === 'Venter'}
-										<Tooltip.Trigger><EnvelopeOpen class="w-4 h-4" /></Tooltip.Trigger>
-										<Tooltip.Content>
-											<p>Venter</p>
-										</Tooltip.Content>
-									{:else}
-										<Tooltip.Trigger
-											><ExclamationTriangle
-												class="w-4 h-4 text-red-800 dark:text-red-400"
-											/></Tooltip.Trigger
-										>
-										<Tooltip.Content>
-											<p>Mangler</p>
-										</Tooltip.Content>
-									{/if}
-								</Tooltip.Root>
-							</div>
-							<Table.Cell class="sm:table-cell">
-								<Tooltip.Root>
-									{#if elevtidNum(opgave['elev-tid']) >= 10}
-										<Tooltip.Trigger>
-											<Badge class="bg-orange-400">{opgave['elev-tid']}</Badge>
-										</Tooltip.Trigger>
-										<Tooltip.Content>
-											<p>Opgaven har {opgave['elev-tid']} elev timer</p>
-										</Tooltip.Content>
-									{:else if elevtidNum(opgave['elev-tid']) >= 5}
-										<Tooltip.Trigger>
-											<Badge class="text-black bg-yellow-400">{opgave['elev-tid']}</Badge>
-										</Tooltip.Trigger>
-										<Tooltip.Content>
-											<p>Opgaven har {opgave['elev-tid']} elev timer</p>
-										</Tooltip.Content>
-									{:else if elevtidNum(opgave['elev-tid']) > 0}
-										<Tooltip.Trigger>
-											<Badge variant="outline" class="border-green-400">{opgave['elev-tid']}</Badge>
-										</Tooltip.Trigger>
-										<Tooltip.Content>
-											<p>
-												Opgaven har {opgave['elev-tid']} elev time{elevtidNum(
-													opgave['elev-tid']
-												) === 1
-													? ''
-													: 'r'}
-											</p>
-										</Tooltip.Content>
-									{:else}
-										<Tooltip.Trigger>
-											<Badge variant="outline">{opgave['elev-tid']}</Badge>
-										</Tooltip.Trigger>
-										<Tooltip.Content>
-											<p>Opgaven har ingen elev timer</p>
-										</Tooltip.Content>
-									{/if}
-								</Tooltip.Root>
-							</Table.Cell>
-							<Table.Cell class="sm:table-cell">
-								{#if opgave.karakter || opgave.elevnote}
-									<Tooltip.Root>
-										<Tooltip.Trigger>
-											<ChatBubble class="w-4 h-4" />
-										</Tooltip.Trigger>
-										<Tooltip.Content class="w-52">
-											{#if opgave.karakter}
-												<p>Karakter: {opgave.karakter}</p>
-											{/if}
-											{#if opgave.karakter && opgave.elevnote}
-												<Separator class="my-2" />
-											{/if}
-											{#if opgave.elevnote}
-												<p>Besked: {opgave.elevnote}</p>
-											{/if}
-										</Tooltip.Content>
-									</Tooltip.Root>
+	<Table.Root class="overflow-x-auto" containerClass="hidden md:block !mt-4">
+		<Table.Header>
+			<Table.Row>
+				<Table.Head></Table.Head>
+				<Table.Head class="sm:table-cell">Elev tid</Table.Head>
+				<Table.Head></Table.Head>
+				<Table.Head class="sm:table-cell">Hold</Table.Head>
+				<Table.Head class="sm:table-cell">Frist</Table.Head>
+				<Table.Head class="sm:table-cell">Opgavetitel</Table.Head>
+			</Table.Row>
+		</Table.Header>
+		<Table.Body>
+			{#if opgaver}
+				{#each opgaver as opgave}
+					<Table.Row
+						on:click={async () => await goto(`/opgave?id=${opgave.exerciseid}`)}
+						class="cursor-pointer select-none"
+					>
+						<!-- handed in -->
+						<div class="flex items-center ml-2 h-9">
+							<Tooltip.Root>
+								{#if opgave.status === 'Afleveret'}
+									<Tooltip.Trigger
+									>
+										<Rocket
+											class="w-4 h-4 text-green-800 dark:text-green-400"
+										/>
+									</Tooltip.Trigger
+									>
+									<Tooltip.Content>
+										<p>Afleveret</p>
+									</Tooltip.Content>
+								{:else if opgave.status === 'Afsluttet'}
+									<Tooltip.Trigger
+									>
+										<Archive class="w-4 h-4 text-blue-800 dark:text-blue-400" />
+									</Tooltip.Trigger
+									>
+									<Tooltip.Content>
+										<p>Afsluttet</p>
+									</Tooltip.Content>
+								{:else if opgave.status === 'Venter'}
+									<Tooltip.Trigger>
+										<EnvelopeOpen class="w-4 h-4" />
+									</Tooltip.Trigger>
+									<Tooltip.Content>
+										<p>Venter</p>
+									</Tooltip.Content>
+								{:else}
+									<Tooltip.Trigger
+									>
+										<ExclamationTriangle
+											class="w-4 h-4 text-red-800 dark:text-red-400"
+										/>
+									</Tooltip.Trigger
+									>
+									<Tooltip.Content>
+										<p>Mangler</p>
+									</Tooltip.Content>
 								{/if}
-							</Table.Cell>
-							<Table.Cell class="text-nowrap line-clamp-1 sm:table-cell">{opgave.hold}</Table.Cell>
-							<Table.Cell class="sm:table-cell text-nowrap">
+							</Tooltip.Root>
+						</div>
+						<Table.Cell class="sm:table-cell">
+							<Tooltip.Root>
+								{#if elevtidNum(opgave['elev-tid']) >= 10}
+									<Tooltip.Trigger>
+										<Badge class="bg-orange-400">{opgave['elev-tid']}</Badge>
+									</Tooltip.Trigger>
+									<Tooltip.Content>
+										<p>Opgaven har {opgave['elev-tid']} elev timer</p>
+									</Tooltip.Content>
+								{:else if elevtidNum(opgave['elev-tid']) >= 5}
+									<Tooltip.Trigger>
+										<Badge class="text-black bg-yellow-400">{opgave['elev-tid']}</Badge>
+									</Tooltip.Trigger>
+									<Tooltip.Content>
+										<p>Opgaven har {opgave['elev-tid']} elev timer</p>
+									</Tooltip.Content>
+								{:else if elevtidNum(opgave['elev-tid']) > 0}
+									<Tooltip.Trigger>
+										<Badge variant="outline" class="border-green-400">{opgave['elev-tid']}</Badge>
+									</Tooltip.Trigger>
+									<Tooltip.Content>
+										<p>
+											Opgaven har {opgave['elev-tid']} elev time{elevtidNum(
+											opgave['elev-tid']
+										) === 1
+											? ''
+											: 'r'}
+										</p>
+									</Tooltip.Content>
+								{:else}
+									<Tooltip.Trigger>
+										<Badge variant="outline">{opgave['elev-tid']}</Badge>
+									</Tooltip.Trigger>
+									<Tooltip.Content>
+										<p>Opgaven har ingen elev timer</p>
+									</Tooltip.Content>
+								{/if}
+							</Tooltip.Root>
+						</Table.Cell>
+						<Table.Cell class="sm:table-cell">
+							{#if opgave.karakter || opgave.elevnote}
 								<Tooltip.Root>
 									<Tooltip.Trigger>
-										<p
-											use:relativeTime={DateTime.fromFormat(
+										<ChatBubble class="w-4 h-4" />
+									</Tooltip.Trigger>
+									<Tooltip.Content class="w-52">
+										{#if opgave.karakter}
+											<p>Karakter: {opgave.karakter}</p>
+										{/if}
+										{#if opgave.karakter && opgave.elevnote}
+											<Separator class="my-2" />
+										{/if}
+										{#if opgave.elevnote}
+											<p>Besked: {opgave.elevnote}</p>
+										{/if}
+									</Tooltip.Content>
+								</Tooltip.Root>
+							{/if}
+						</Table.Cell>
+						<Table.Cell class="text-nowrap line-clamp-1 sm:table-cell">{opgave.hold}</Table.Cell>
+						<Table.Cell class="sm:table-cell text-nowrap">
+							<Tooltip.Root>
+								<Tooltip.Trigger>
+									<p
+										use:relativeTime={DateTime.fromFormat(
 												opgave.frist,
 												'd/M-yyyy HH:mm'
 											).toJSDate()}
-										/>
-									</Tooltip.Trigger>
-									<Tooltip.Content>
-										{opgave.frist}
-									</Tooltip.Content>
-								</Tooltip.Root>
-							</Table.Cell>
-							<Table.Cell class="text-nowrap line-clamp-1 sm:table-cell"
-								>{opgave.opgavetitel}</Table.Cell
-							>
-						</Table.Row>
-					{/each}
-				{/if}
-			</Table.Body>
-		</Table.Root>
-	{:else}
-		<!-- mobile version here, based on list not table, without tooltips -->
-		<div class="flex flex-col w-full gap-2">
-			{#if opgaver}
-				{#each opgaver as opgave}
-					<button
-						on:click={async () => await goto(`/opgave?id=${opgave.exerciseid}`)}
-						class="flex items-center w-full justify-between p-2 border-[1px] gap-2 rounded-md shadow-md cursor-pointer border-border"
-					>
-						<div class="flex items-start space-x-2">
-							<div class="flex items-start justify-start py-1">
-								{#if opgave.status === 'Afleveret'}
-									<Rocket class="w-4 h-4 text-green-800 dark:text-green-400" />
-								{:else if opgave.status === 'Afsluttet'}
-									<Archive class="w-4 h-4 text-blue-800 dark:text-blue-400" />
-								{:else if opgave.status === 'Venter'}
-									<EnvelopeOpen class="w-4 h-4" />
-								{:else}
-									<ExclamationTriangle class="w-4 h-4 text-red-800 dark:text-red-400" />
-								{/if}
-							</div>
-							<div class="flex flex-col items-start justify-center">
-								<p class="text-sm font-semibold text-left">{opgave.opgavetitel}</p>
-								<p class="text-xs text-gray-500">{opgave.hold}</p>
-							</div>
-						</div>
-						<div class="flex items-center space-x-2">
-							{#if opgave.karakter || opgave.elevnote}
-								<ChatBubble class="w-4 h-4" />
-							{/if}
-							<div class="flex flex-col items-end justify-center">
-								<p class="text-xs text-gray-500 text-nowrap">
-									{DateTime.fromFormat(opgave.frist, 'd/M-yyyy HH:mm').toRelative()}
-								</p>
-								<p class="text-xs text-gray-500 text-nowrap">
-									{opgave['elev-tid']} elev time{elevtidNum(opgave['elev-tid']) === 1 ? '' : 'r'}
-								</p>
-							</div>
-						</div>
-					</button>
+									/>
+								</Tooltip.Trigger>
+								<Tooltip.Content>
+									{opgave.frist}
+								</Tooltip.Content>
+							</Tooltip.Root>
+						</Table.Cell>
+						<Table.Cell class="text-nowrap line-clamp-1 sm:table-cell"
+						>{opgave.opgavetitel}</Table.Cell
+						>
+					</Table.Row>
 				{/each}
 			{/if}
-		</div>
-	{/if}
+		</Table.Body>
+	</Table.Root>
+	<!-- mobile version here, based on list not table, without tooltips -->
+	<div class="max-md:flex md:hidden flex-col w-full gap-2">
+		{#if opgaver}
+			{#each opgaver as opgave}
+				<button
+					on:click={async () => await goto(`/opgave?id=${opgave.exerciseid}`)}
+					class="flex items-center w-full justify-between p-2 border-[1px] gap-2 rounded-md shadow-md cursor-pointer border-border"
+				>
+					<div class="flex items-start space-x-2">
+						<div class="flex items-start justify-start py-1">
+							{#if opgave.status === 'Afleveret'}
+								<Rocket class="w-4 h-4 text-green-800 dark:text-green-400" />
+							{:else if opgave.status === 'Afsluttet'}
+								<Archive class="w-4 h-4 text-blue-800 dark:text-blue-400" />
+							{:else if opgave.status === 'Venter'}
+								<EnvelopeOpen class="w-4 h-4" />
+							{:else}
+								<ExclamationTriangle class="w-4 h-4 text-red-800 dark:text-red-400" />
+							{/if}
+						</div>
+						<div class="flex flex-col items-start justify-center">
+							<p class="text-sm font-semibold text-left">{opgave.opgavetitel}</p>
+							<p class="text-xs text-gray-500">{opgave.hold}</p>
+						</div>
+					</div>
+					<div class="flex items-center space-x-2">
+						{#if opgave.karakter || opgave.elevnote}
+							<ChatBubble class="w-4 h-4" />
+						{/if}
+						<div class="flex flex-col items-end justify-center">
+							<p class="text-xs text-gray-500 text-nowrap">
+								{DateTime.fromFormat(opgave.frist, 'd/M-yyyy HH:mm').toRelative()}
+							</p>
+							<p class="text-xs text-gray-500 text-nowrap">
+								{opgave['elev-tid']} elev time{elevtidNum(opgave['elev-tid']) === 1 ? '' : 'r'}
+							</p>
+						</div>
+					</div>
+				</button>
+			{/each}
+		{/if}
+	</div>
 </div>
